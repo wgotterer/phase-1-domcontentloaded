0a7e0040eea909b4976ac9e8c43432c11a30f91a
William Gotterer <william.gotterer@gmail.com>
2021-09-26 08:39:24 -0400

    commit tree f38b6be3940e097b04f702a9112193a1bf89015f

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    replaceText = document.querySelector("#text")
+   replaceText.innerHTML="This is really cool!"
+  });
+
+  replaceText = document.querySelector("#text")
+  replaceText.innerHTML="This is really cool!"
